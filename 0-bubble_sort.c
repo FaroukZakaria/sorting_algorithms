eabb91208f1de599ff114cdc4d7d2c7ee7d1bd45
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-07-25 19:20:36 +0000

    task answer

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,11 +14,11 @@ void bubble_sort(int *array, size_t size)
 		flag = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] < array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
-				swapped = 1;
+				flag = 1;
 			}
 		}
 		if (flag == 0)
