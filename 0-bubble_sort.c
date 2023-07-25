3c621da26ebf2bc6ab3ab99830311f95222ee0cb
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-07-25 19:07:01 +0000

    task answer

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * bubble_sort - s
+ * @array: s
+ * @size: s
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int flag;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] < array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+}
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
